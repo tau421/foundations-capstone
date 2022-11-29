eaa41e283b3bfa370c59cd12345e4075f73e890d
Kyle Wilson <tau421@gmail.com>
2022-11-28 23:34:06 -0600

    working on cursors change

---
+++ public/index.js
@@ -0,0 +1,9 @@
+// const bigGreenImage = document.getElementById("bigGreenImage")
+
+// const changeCursor = () => {
+//     document.body.classList.add('cursor-bigGreen');
+//     console.log('image selected')
+//     // document.body.style.cursor = "url(https://i.ibb.co/RC82FXJ/bigGreen.png), pointer"
+// }
+
+// bigGreenImage.addEventListener('click', changeCursor)
--- server/index.js
+++ server/index.js
@@ -6,4 +6,6 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json());
+
 app.listen(5500, () => console.log('Server running on 5500'));
