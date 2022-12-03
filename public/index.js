da27ab7a2ab01754803e30cea99e4e4cc39cd5d8
Kyle Wilson <tau421@gmail.com>
2022-12-03 12:32:32 -0600

    red and yellow done

--- public/index.js
+++ public/index.js
@@ -49,32 +49,64 @@ const redSoap1 = () => {
     document.getElementById("carpet1").innerHTML = '<img src="https://i.ibb.co/KbrKJTR/red-Soap-transparent.png" alt="red-Soap-transparent" id="redSoap1" onclick="cleanCarpet1()">'
     increaseScore()
 }
+const yellowSoap2 = () => {
+    document.getElementById("carpet2").innerHTML = '<img src="https://i.ibb.co/2Myv0Zw/yellow-Soap-transparent.png" alt="yellow-Soap-transparent" id="yellowSoap2" onclick="cleanCarpet2()">'
+    increaseScore()
+}
 const redSoap4 = () => {
     document.getElementById("carpet4").innerHTML = '<img src="https://i.ibb.co/KbrKJTR/red-Soap-transparent.png" alt="red-Soap-transparent" id="redSoap4" onclick="cleanCarpet4()">'
+    increaseScore()
+}
+const yellowSoap5 = () => {
+    document.getElementById("carpet5").innerHTML = '<img src="https://i.ibb.co/2Myv0Zw/yellow-Soap-transparent.png" alt="yellow-Soap-transparent" id="yellowSoap5" onclick="cleanCarpet5()">'
     increaseScore()
 }
 const redSoap7 = () => {
     document.getElementById("carpet7").innerHTML = '<img src="https://i.ibb.co/KbrKJTR/red-Soap-transparent.png" alt="red-Soap-transparent" id="redSoap7" onclick="cleanCarpet7()">'
     increaseScore()
 }
+const yellowSoap8 = () => {
+    document.getElementById("carpet8").innerHTML = '<img src="https://i.ibb.co/2Myv0Zw/yellow-Soap-transparent.png" alt="yellow-Soap-transparent" id="yellowSoap8" onclick="cleanCarpet8()">'
+    increaseScore()
+}
 const redSoap10 = () => {
     document.getElementById("carpet10").innerHTML = '<img src="https://i.ibb.co/KbrKJTR/red-Soap-transparent.png" alt="red-Soap-transparent" id="redSoap10" onclick="cleanCarpet10()">'
+    increaseScore()
+}
+const yellowSoap11 = () => {
+    document.getElementById("carpet11").innerHTML = '<img src="https://i.ibb.co/2Myv0Zw/yellow-Soap-transparent.png" alt="yellow-Soap-transparent" id="yellowSoap11" onclick="cleanCarpet11()">'
     increaseScore()
 }
 const redSoap13 = () => {
     document.getElementById("carpet13").innerHTML = '<img src="https://i.ibb.co/KbrKJTR/red-Soap-transparent.png" alt="red-Soap-transparent" id="redSoap13" onclick="cleanCarpet13()">'
     increaseScore()
 }
+const yellowSoap14 = () => {
+    document.getElementById("carpet14").innerHTML = '<img src="https://i.ibb.co/2Myv0Zw/yellow-Soap-transparent.png" alt="yellow-Soap-transparent" id="yellowSoap14" onclick="cleanCarpet14()">'
+    increaseScore()
+}
 const redSoap16 = () => {
     document.getElementById("carpet16").innerHTML = '<img src="https://i.ibb.co/KbrKJTR/red-Soap-transparent.png" alt="red-Soap-transparent" id="redSoap16" onclick="cleanCarpet16()">'
+    increaseScore()
+}
+const yellowSoap17 = () => {
+    document.getElementById("carpet17").innerHTML = '<img src="https://i.ibb.co/2Myv0Zw/yellow-Soap-transparent.png" alt="yellow-Soap-transparent" id="yellowSoap17" onclick="cleanCarpet17()">'
     increaseScore()
 }
 const redSoap19 = () => {
     document.getElementById("carpet19").innerHTML = '<img src="https://i.ibb.co/KbrKJTR/red-Soap-transparent.png" alt="red-Soap-transparent" id="redSoap19" onclick="cleanCarpet19()">'
     increaseScore()
 }
+const yellowSoap20 = () => {
+    document.getElementById("carpet20").innerHTML = '<img src="https://i.ibb.co/2Myv0Zw/yellow-Soap-transparent.png" alt="yellow-Soap-transparent" id="yellowSoap20" onclick="cleanCarpet20()">'
+    increaseScore()
+}
 const redSoap22 = () => {
     document.getElementById("carpet22").innerHTML = '<img src="https://i.ibb.co/KbrKJTR/red-Soap-transparent.png" alt="red-Soap-transparent" id="redSoap22" onclick="cleanCarpet22()">'
+    increaseScore()
+}
+const yellowSoap23 = () => {
+    document.getElementById("carpet23").innerHTML = '<img src="https://i.ibb.co/2Myv0Zw/yellow-Soap-transparent.png" alt="yellow-Soap-transparent" id="yellowSoap23" onclick="cleanCarpet23()">'
     increaseScore()
 }
 const redSoap25 = () => {
@@ -86,32 +118,64 @@ const cleanCarpet1 = () => {
     document.getElementById("redSoap1").style.visibility = 'hidden'
     increaseScore()
 }
+const cleanCarpet2 = () => {
+    document.getElementById("yellowSoap2").style.visibility = 'hidden'
+    increaseScore()
+}
 const cleanCarpet4 = () => {
     document.getElementById("redSoap4").style.visibility = 'hidden'
+    increaseScore()
+}
+const cleanCarpet5 = () => {
+    document.getElementById("yellowSoap5").style.visibility = 'hidden'
     increaseScore()
 }
 const cleanCarpet7 = () => {
     document.getElementById("redSoap7").style.visibility = 'hidden'
     increaseScore()
 }
+const cleanCarpet8 = () => {
+    document.getElementById("yellowSoap8").style.visibility = 'hidden'
+    increaseScore()
+}
 const cleanCarpet10 = () => {
     document.getElementById("redSoap10").style.visibility = 'hidden'
+    increaseScore()
+}
+const cleanCarpet11 = () => {
+    document.getElementById("yellowSoap11").style.visibility = 'hidden'
     increaseScore()
 }
 const cleanCarpet13 = () => {
     document.getElementById("redSoap13").style.visibility = 'hidden'
     increaseScore()
 }
+const cleanCarpet14 = () => {
+    document.getElementById("yellowSoap14").style.visibility = 'hidden'
+    increaseScore()
+}
 const cleanCarpet16 = () => {
     document.getElementById("redSoap16").style.visibility = 'hidden'
+    increaseScore()
+}
+const cleanCarpet17 = () => {
+    document.getElementById("yellowSoap17").style.visibility = 'hidden'
     increaseScore()
 }
 const cleanCarpet19 = () => {
     document.getElementById("redSoap19").style.visibility = 'hidden'
     increaseScore()
 }
+const cleanCarpet20 = () => {
+    document.getElementById("yellowSoap20").style.visibility = 'hidden'
+    increaseScore()
+}
 const cleanCarpet22 = () => {
     document.getElementById("redSoap22").style.visibility = 'hidden'
+    increaseScore()
+}
+const cleanCarpet23 = () => {
+    document.getElementById("yellowSoap23").style.visibility = 'hidden'
     increaseScore()
 }
 const cleanCarpet25 = () => {
